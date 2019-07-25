2131976e1de2482a655664b9f3c5678f37c3b2d3
clyhs <clygd@126.com>
2019-07-25 17:21:33 +0800

    gateway

--- cloudpaas-admin/src/main/java/com/springcloud/admin/controller/UserController.java
+++ cloudpaas-admin/src/main/java/com/springcloud/admin/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @Api
 @RestController
-@RequestMapping("admin/user")
+@RequestMapping("user")
 public class UserController {
 
     @GetMapping("/lists")
