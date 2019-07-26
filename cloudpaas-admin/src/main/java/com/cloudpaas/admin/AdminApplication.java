4acc569872c22a6b46e4f7cfbecd3fca97269c16
clyhs <clygd@126.com>
2019-07-26 16:41:42 +0800

    del security

--- cloudpaas-admin/src/main/java/com/cloudpaas/admin/AdminApplication.java
+++ cloudpaas-admin/src/main/java/com/cloudpaas/admin/AdminApplication.java
@@ -31,15 +31,15 @@ public class AdminApplication {
         SpringApplication.run(AdminApplication.class, args);
     }
 
-    @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                .and().csrf().disable();
-        }
-    }
-    
+//    @Configuration
+//    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.authorizeRequests().anyRequest().permitAll()
+//                .and().csrf().disable();
+//        }
+//    }
+//    
     
 
 }
