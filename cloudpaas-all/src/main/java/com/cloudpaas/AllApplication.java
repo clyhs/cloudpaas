4bc40946507df237bc6e4dc90a0ec7c5fcb3e872
clyhs <clygd@126.com>
2019-07-26 16:18:09 +0800

    mybatis

--- cloudpaas-all/src/main/java/com/cloudpaas/AllApplication.java
+++ cloudpaas-all/src/main/java/com/cloudpaas/AllApplication.java
@@ -3,8 +3,10 @@
  */
 package com.cloudpaas;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 
@@ -14,6 +16,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019年7月26日 下午12:42:12
  */
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.cloudpaas.admin.mapper")
 public class AllApplication {
 
 	public static void main(String[] args) {
