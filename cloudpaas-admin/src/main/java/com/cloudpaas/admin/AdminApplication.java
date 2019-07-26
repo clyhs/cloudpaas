08d8664a35d2efb1431025a7589a042e12b21214
clyhs <clygd@126.com>
2019-07-26 23:03:17 +0800

    boot

--- cloudpaas-admin/src/main/java/com/cloudpaas/admin/AdminApplication.java
+++ cloudpaas-admin/src/main/java/com/cloudpaas/admin/AdminApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import org.mybatis.spring.annotation.MapperScan;
