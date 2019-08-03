7d2f3c93c5f73195c5f4a4af77548c100280a334
clyhs <clygd@126.com>
2019-08-04 00:53:12 +0800

    common pom

---
+++ cloudpaas-admin-ui/src/main/java/com/cloudpaas/admin/ui/App.java
@@ -0,0 +1,13 @@
+package com.cloudpaas.admin.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
