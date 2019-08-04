e557b0c5c14df37d5fca95a11d6a6aed5d3ad90e
clyhs <clygd@126.com>
2019-08-04 22:40:36 +0800

    admin-ui

---
+++ cloudpaas-admin-ui/src/main/java/com/cloudpaas/admin/ui/AdminUIApplication.java
@@ -0,0 +1,19 @@
+package com.cloudpaas.admin.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author 大鱼
+ *
+ * @date 2019年8月4日 下午10:35:28
+ */
+@SpringBootApplication
+public class AdminUIApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(AdminUIApplication.class, args);
+    }
+}
---
+++ cloudpaas-admin-ui/src/main/java/com/cloudpaas/admin/ui/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.cloudpaas.admin.ui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ *
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminUIApplication.class);
+    }
+}
