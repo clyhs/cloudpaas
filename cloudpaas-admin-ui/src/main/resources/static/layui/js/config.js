73839505aff1aef3e8fcccd51418f98ebdb45f16
clyhs <clygd@126.com>
2019-08-13 20:01:32 +0800

    config.js

--- cloudpaas-admin-ui/src/main/resources/static/layui/js/config.js
+++ cloudpaas-admin-ui/src/main/resources/static/layui/js/config.js
@@ -1,4 +1,4 @@
 var $config = {
     apiContext: 'http://localhost:8200' ,  // api请求地址
-    context: '/adminui'                    // 前端资源访问根目录,生产环境请设置为托管前端资源的位置
+    context: '/adminui'                    // 前端上下文路径
 };
