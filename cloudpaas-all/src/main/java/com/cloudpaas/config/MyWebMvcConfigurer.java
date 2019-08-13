24a6fa5b3aacc914d9b9e28935fdd921075d3a78
clyhs <clygd@126.com>
2019-08-13 19:11:14 +0800

    admin ui

--- cloudpaas-all/src/main/java/com/cloudpaas/config/MyWebMvcConfigurer.java
+++ cloudpaas-all/src/main/java/com/cloudpaas/config/MyWebMvcConfigurer.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer  {
 
+	/**
+	 * 支持跨域，否则admin-ui请求不到数据
+	 */
     @Override  
     public void addCorsMappings(CorsRegistry registry) {  
         registry.addMapping("/**")
