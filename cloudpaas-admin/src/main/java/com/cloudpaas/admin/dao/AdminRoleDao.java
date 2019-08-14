b32d6b3816d53b431f47af80e96526f27b2af1d6
clyhs <clygd@126.com>
2019-08-14 14:45:16 +0800

    roledao

--- cloudpaas-admin/src/main/java/com/cloudpaas/admin/dao/AdminRoleDao.java
+++ cloudpaas-admin/src/main/java/com/cloudpaas/admin/dao/AdminRoleDao.java
@@ -3,6 +3,8 @@
  */
 package com.cloudpaas.admin.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.cloudpaas.admin.mapper.AdminRoleMapper;
 import com.cloudpaas.common.base.dao.BaseDao;
 import com.cloudpaas.common.model.Role;
@@ -12,6 +14,7 @@ import com.cloudpaas.common.model.Role;
  *
  * @date 2019年8月14日 下午2:41:07
  */
+@Repository
 public class AdminRoleDao extends BaseDao<AdminRoleMapper, Role> {
 
 }
