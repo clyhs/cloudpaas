6d0bfc6b83b0b237123452f698d59d520576d16a
clyhs <clygd@126.com>
2019-08-17 15:03:04 +0800

    shop service

---
+++ cloudpaas-service-shop/src/main/java/com/cloudpaas/service/shop/ShopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudpaas.service.shop;
+
+/**
+ * Hello world!
+ *
+ */
+public class ShopServiceApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
