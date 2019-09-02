5d20fe5f9808cee8e0afa98c6dff31ebe8f26c48
clyhs <clygd@126.com>
2019-09-02 17:21:39 +0800

    mybatis plugin

--- cloudpaas-service-pas/src/main/java/com/cloudpaas/service/pas/PasServiceApplication.java
+++ cloudpaas-service-pas/src/main/java/com/cloudpaas/service/pas/PasServiceApplication.java
@@ -11,15 +11,8 @@ import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.cloudpaas.common.EnableAPISwagger2;
 
